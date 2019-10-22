e215c0743f47deb1176d8363116ef0d7163ef602
Alina Shokota <alina.shokota159@gmail.com>
2019-10-22 23:18:47 +0200

    add endpoints

--- src/app/service/noun.service.ts
+++ src/app/service/noun.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Noun } from '../model/noun';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NounService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllNouns(): Observable<Noun[]> {
+    return this.http.get<Noun[]>('http://localhost:8080/nouns/all');
+  }
+
+  get(id: number): Observable<Noun> {
+    return this.http.get<Noun>('http://localhost:8080/nouns/get/'+id);
+  }
+
+  getRandom(): Observable<Noun> {
+    return this.http.get<Noun>('http://localhost:8080/nouns/get/random');
+  }
+
+  save(noun: Noun): Observable<Object> {
+    return this.http.post('http://localhost:8080/nouns/save', noun);
+  }
+
+  delete(id: number): Observable<Noun> {
+    return this.http.delete<Noun>('http://localhost:8080/nouns/delete/'+id);
+  }
 }
--- src/app/service/verb.service.ts
+++ src/app/service/verb.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Verb } from '../model/verb';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerbService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllVerbs(): Observable<Verb[]> {
+    return this.http.get<Verb[]>('http://localhost:8080/verbs/all');
+  }
+
+  get(id: number): Observable<Verb> {
+    return this.http.get<Verb>('http://localhost:8080/verbs/get/'+id);
+  }
+
+  getRandom(): Observable<Verb> {
+    return this.http.get<Verb>('http://localhost:8080/verbs/get/random');
+  }
+
+  save(verb: Verb): Observable<Object> {
+    return this.http.post('http://localhost:8080/verbs/save', verb);
+  }
+
+  delete(id: number): Observable<Verb> {
+    return this.http.delete<Verb>('http://localhost:8080/verbs/delete/'+id);
+  }
 }
